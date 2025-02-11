8d7f134ebe6ea3b1d7391839574e91da3dac4f84
Inesa Kostova <kostovainesa@gmail.com>
2025-02-11 14:00:13 +0200

    Add files via upload

---
+++ Area of Figures.cs
@@ -0,0 +1,29 @@
+﻿string typeFigures = Console.ReadLine();
+
+double area = 0;
+
+if (typeFigures == "square")
+{
+    double sideA = double.Parse(Console.ReadLine());
+    area = sideA * sideA;
+}
+else if (typeFigures == "rectangle")
+{
+    double sideA = double.Parse(Console.ReadLine());
+    double sideB = double.Parse(Console.ReadLine());
+    area = sideA * sideB;
+}
+else if (typeFigures == "circle")
+{
+    double radius = double.Parse(Console.ReadLine());
+    area = Math.PI * radius * radius;
+}
+else if (typeFigures == "triangle")
+{
+    double sideA = double.Parse(Console.ReadLine());
+    double heighA = double.Parse(Console.ReadLine());
+    area = sideA * heighA / 2;
+}
+Console.WriteLine($"{area:F3}");
+
+
